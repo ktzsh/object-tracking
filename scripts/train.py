d979cd2de9ccd0c92897ec30b3cfeb65330fefc7
Kshitiz Sharma <kshitizsharma38@gmail.com>
2017-08-08 21:20:56 +0530

    Train scripts updated

--- scripts/train.py
+++ scripts/train.py
@@ -12,18 +12,21 @@ def get_model():
 
     lstm_units = 512
     sequence_length = 6
-    feat_size = 5120 # 4096(img-feat) + 1024(heat-map)
+    heatmap_size = 32*32
+    feat_size = 1024 + heatmap_size # 1024(img-feat) + 1024(heat-map)
 
     x_input = Input(shape=(sequence_length, feat_size), dtype='float32', name='img_heatmap_feat')
     x = LSTM(lstm_units, return_sequences=False, implementation=2, name='recurrent_layer')(x_input)
-    output = TimeDistributed(Dense(4, activation='sigmoid'), name='location')(x)
+    output = TimeDistributed(Dense(heatmap_size, activation='sigmoid'), name='location')(x)
 
     model = Model(inputs=x_input, outputs=output)
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
+    return model
 
-def train(x_train, y_train):
+def train(x_train, y_train, x_val, y_val):
 
+    model = get_model()
     checkpointer = ModelCheckpoint(filepath='weights/weights.{epoch:02d}-{val_loss:.2f}.hdf5', verbose=1, save_best_only=True)
     earlystopper = EarlyStopping(monitor='val_loss', patience=15, verbose=1, mode='auto')
-    model.fit(x_train, y_train, validation_split=0.2, batch_size=64, epochs=100, verbose=0, shuffle=True, callbacks=[checkpointer, earlystopper])
+    model.fit(x_train, y_train, validation_data=(x_val,y_val), batch_size=64, epochs=100, verbose=0, shuffle=False, callbacks=[checkpointer, earlystopper])
--- utils/det_utils.py
+++ utils/det_utils.py
@@ -24,7 +24,7 @@ net = load_net("cfg/coco.data", "cfg/yolo.cfg", "yolo.weights")
 global net_dim
 net_dim = 608
 
-def prepare_data(data_dirs = ['Human2','Human3','Human4','Human5','Human5','Human7','Human8','Human9','Woman','Jogging-1', 'Jogging-2','Walking','Walking2']):
+def prepare_data(data_dirs = ['Human2','Human3','Human4','Human5','Human6','Human7','Human8','Human9','Woman','Jogging-1', 'Jogging-2','Walking','Walking2']):
     path_prefix = '/root/Artifacia-Data/TB-50/'
     frame_paths_dirs, frame_bboxs_dirs, frame_dim_dirs = [], [], []
     print "All Directories List:", data_dirs
@@ -47,7 +47,7 @@ def prepare_data(data_dirs = ['Human2','Human3','Human4','Human5','Human5','Huma
                     frame = cv2.imread(frame_path)
                     frame_height, frame_width, frame_channel = frame.shape[0], frame.shape[1], frame.shape[2]
 
-                if data_dir=='Jogging-1' or data_dir=='Jogging-2':
+                if data_dir=='Jogging-1' or data_dir=='Jogging-2' or data_dir=='Woman' or data_dir=='Walking' or data_dir=='Walking2':
                     frame_bbox = line.rstrip('\n').split()
                     frame_bboxs.append(frame_bbox)
                 else:
@@ -82,15 +82,22 @@ def generate_heatmap_feat(det_x, det_y, det_h, det_w, smap=32):
     return heatmap_feat
 
 
-def process_data(frame_paths_dirs, frame_bboxs_dirs, frame_dim_dirs, data_dirs = ['Human2','Human3','Human4','Human5','Human5','Human7','Human8','Human9','Woman','Jogging-1','Jogging-2','Walking','Walking2']):
+def process_data(frame_paths_dirs, frame_bboxs_dirs, frame_dim_dirs, data_dirs = ['Human2','Human3','Human4','Human5','Human6','Human7','Human8','Human9','Woman','Jogging-1','Jogging-2','Walking','Walking2']):
     for i, (frame_paths, frame_bboxs, frame_dim) in enumerate(zip(frame_paths_dirs, frame_bboxs_dirs, frame_dim_dirs)):
         frame_height, frame_width = frame_dim[0], frame_dim[1]
 
-        vis_feat = np.zeros((len(frame_paths), 1024))
-        heatmap_feat = np.zeros((len(frame_paths), 1024))
+        vis_data_file = '../data/vis_feats_' + data_dirs[i]
+        heatmap_data_file = '../data/heatmap_feats_' + data_dirs[i]
+
+        # if i<0:
+        #     continue
 
         print "Extracting features for Data in Directory:", data_dirs[i].split('-')[0]
         for j, (frame_path, frame_bbox) in enumerate(zip(frame_paths,frame_bboxs)):
+
+            # if j<0:
+            #     continue
+
             _, feat = extract_spatio_info(frame_path)
 
             tmp = np.array(feat, dtype='float32')
@@ -102,8 +109,63 @@ def process_data(frame_paths_dirs, frame_bboxs_dirs, frame_dim_dirs, data_dirs =
             det_h = float(frame_bbox[3])/frame_height
             det_w = float(frame_bbox[2])/frame_width
 
-            heat_map_feat = generate_heatmap_feat(det_x, det_y, det_h, det_w)
-            heatmap_feat[j,:] = heat_map_feat
-            vis_feat[j, :] = feat
+            heatmap_feat = generate_heatmap_feat(det_x, det_y, det_h, det_w)
+            np.savetxt(f_vis, (feat.reshape((1,-1))), delimiter=',')
+            np.savetxt(f_heat, (heatmap_feat.reshape((1,-1))), delimiter=',')
 
-        np.savez(data_dirs[i], vis_feat=vis_feat, heatmap_feat=heatmap_feat)
+
+def read_data(sequence_length=6, vis_feat_size=1024, heatmap_feat_size=1024, data_dirs = ['Human2','Human3','Human4','Human5','Human6','Human7','Human8','Human9','Woman','Jogging-1','Jogging-2','Walking','Walking2']):
+    for data_dir in data_dirs:
+
+        print "Reading Directory:", data_dir
+        vis_data_file = '../data/vis_feats_' + data_dir
+        heatmap_data_file = '../data/heatmap_feats_' + data_dir
+
+        vis_feat = np.genfromtxt(vis_data_file, dtype='float32', delimiter=',')
+        heatmap_feat = np.genfromtxt(heatmap_data_file, dtype='float32', delimiter=',')
+
+        leng = vis_feat.shape[0]
+        x = np.zeros((leng - sequence_length, sequence_length, (vis_feat_size + heatmap_feat_size)))
+        y = np.zeros((leng - sequence_length, sequence_length, heatmap_feat_size))
+
+        for i in range(leng-sequence_length):
+
+            x_sample = np.zeros((sequence_length, (vis_feat_size + heatmap_feat_size)))
+            y_sample = np.zeros((sequence_length, heatmap_feat_size))
+            for j in range(sequence_length):
+                x_sample[j, :] = np.append(vis_feat_size[i+j], heatmap_feat[i+j])
+                y_sample[j, :] = heatmap_feat[i+j+1]
+
+            x[i] = x_sample
+            y[i] = y_sample
+
+        yield (x,y)
+
+def get_trainval_data(data_dirs = ['Human2','Human3','Human4','Human5','Human6','Human7','Human8','Human9','Woman','Jogging-1','Jogging-2','Walking','Walking2'], val_data_dirs=['Human3','Human8','Jogging-2','Walking2']):
+
+    x_train = None
+    y_train = None
+    x_val = None
+    y_val = None
+
+    val_index = [data_dirs.index(data_dir) for data_dir in val_data_dirs]
+    data_generator = read_data()
+    for i,(x,y) in enumerate(data_generator):
+
+        if i in val_index:
+            if x_val==None and y_val==None:
+                x_val = x
+                y_val = y
+            else:
+                x_val = np.append(x_val, x, axis=0)
+                y_val = np.append(y_val, y, axis=0)
+        else:
+            if x_train==None and y_train==None:
+                x_train = x
+                y_train = y
+            else:
+                x_train = np.append(x_train, x, axis=0)
+                y_train = np.append(y_train, y, axis=0)
+
+    print "Shapes of Train/Val X/Y Data:", x_train.shape, y_train.shape, x_val.shape, y_val.shape
+    return x_train, y_train, x_val, y_val
